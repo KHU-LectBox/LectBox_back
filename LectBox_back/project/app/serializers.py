a4a3ba539a23b12d7ba0ae36567b9ac0d7ebfae4
GoByeonghu <byeonghugo@gmail.com>
2022-05-28 18:14:03 +0900

    Complete JWT authentication

--- LectBox_back/project/app/serializers.py
+++ LectBox_back/project/app/serializers.py
@@ -1,9 +1,49 @@
 from rest_framework import serializers
 from app.models import Users
 
-# 회원가입시 사용
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from app.models import Users
+
+from django.contrib.auth.models import update_last_login
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+
+
 # 유저 모든 정보 포함
-class SignUptSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = ['u_id', 'pw', 'is_student', 'name', 'email', 'school', 'department']
+        fields = ['user.username', 'user.password', 'is_student', 'name', 'email', 'school', 'department']
+
+
+def ttt(username, password):
+    user = Users.objects.filter(username = username).filter(password= password)
+    return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=20)
+    password = serializers.CharField(max_length=20, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        id = data.get("id", None)
+        password = data.get("pw", None)
+        user = authenticate(username=id, password=password)
+        print(f'로그인 성공? : {user}')
+        if user is None:
+            return {
+                'id': 'None'
+            }
+        try:
+            #update_last_login(None, user)
+            token = Token.objects.get_or_create(user=user)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                'User with given id and password does not exists'
+            )
+        return {
+            'id': user.username,
+            'token': token
+        }
--- LectBox_back/project/app/views.py
+++ LectBox_back/project/app/views.py
@@ -1,50 +1,90 @@
 from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt 
 from rest_framework.parsers import JSONParser
 from app.models import Users
-from app.serializers import SignUptSerializer
+from app.serializers import UserSerializer
+
+from django.shortcuts import render
+#from yaml import serialize
+
+# Create your views here.
+from rest_framework.authtoken.views import ObtainAuthToken, APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from .serializers import UserLoginSerializer
+
+from rest_framework import status
+
+from rest_framework.decorators import api_view
+
+#존재하는 id인지 확인
+def isExist(id):
+    return User.objects.filter(username=id).exists()
+
+
+#회원가입
+class SignupView(APIView):
+    def post(self, request):
+        #아이디 중복 검사
+        if(isExist(request.data['id'])):
+            return Response({"이미 존재하는 id"},status=403)
+        #회원정보 저장    
+        user = User.objects.create_user(username=request.data['id'], password=request.data['pw'])
+        users = Users(user=user, is_student=request.data['is_student'] , name=request.data['name'], email=request.data['email'], school=request.data['school'], department=request.data['department'])
+        user.save()
+        users.save()
+
+        return Response({"ok"},status=200)
+
+        '''
+        #회원 가입과 로그인이 동시 진행 되는 경우
+        token = Token.objects.create(user=user)
+        return Response({"Token": token.key})
+        '''
+#로그인
+class LoginView(APIView):
+    def post(self, request):
+        user = authenticate(username=request.data['id'], password=request.data['pw'])
+        if user is not None:
+            token = Token.objects.create(user=user)
+            #token = Token.objects.get(user=user)
+            return Response({"Token": token.key}, status=200)
+        else:
+            return Response({"'id 또는 pw가 일치하지 않습니다."},status=401)
 
 @csrf_exempt
+@api_view(['POST'])
 def user_list(request):
     """
     유저 전체 정보 이용
     """
-    # 회원 가입
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        print("log01")
-        print(data)
-        serializer = SignUptSerializer(data= data)
-        print("log02")
-        if serializer.is_valid():
-            print("log03")
-            serializer.save()
-            print("log04")
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-    
-    #데이터 삽입여부 테스트용
+    #유저 데이터 삽입여부 테스트용
     if request.method == 'GET':
         user = Users.objects.all()
-        serializer = SignUptSerializer(user, many =True)
+        serializer = UserSerializer(user, many =True)
         return JsonResponse(serializer.data, safe=False)
        
     
 
 @csrf_exempt
-def user_detail(request, userid):
+@api_view(['GET','PUT', 'DELETE'])
+def user_detail(request):
     """
     특정 유저 정보 이용
     """
     try:
-        user = Users.objects.get(u_id=userid)
+        user = User.objects.get(username=request.user)
     except Users.DoesNotExist:
         return HttpResponse(status=404)
+    
+    users = Users.objects.get(user = user)
 
     #유저 정보 획득
     if request.method == 'GET':
-        serializer = SignUptSerializer(user)
-        return JsonResponse(serializer.data)
+        return Response({"id": user.username, "is_student": users.is_student, "name":users.name, "email":users.email, "school":users.school, "department":users.department}, status=200)
 
 '''
     #유저 정보 수정
@@ -61,3 +101,6 @@ def user_detail(request, userid):
         snippet.delete()
         return HttpResponse(status=204)
 '''
+
+
+
