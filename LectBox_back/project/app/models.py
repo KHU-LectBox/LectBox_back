e728c4270cc3181afbe72d783777c72522621aaf
hoo <bhwkd@naver.com>
2022-05-28 18:14:03 +0900

    Complete JWT authentication

--- LectBox_back/project/app/models.py
+++ LectBox_back/project/app/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#from rest_framework.authtoken.models import Token
+
 
 class Users(models.Model):
     user = models.OneToOneField(User, null=True ,  on_delete=models.CASCADE)
 
-    u_id = models.CharField(max_length=20,blank=False, null= False, unique=True, primary_key= True)
-    pw = models.CharField(max_length=20,blank=False, null= False)
     is_student = models.BooleanField(blank=False, null=False)
     name = models.CharField(max_length=20,blank=False, null= False)
     email = models.TextField(blank=False, null= False)
     school = models.TextField(blank=False, null= False)
     department = models.TextField(blank=False, null= False)
 
-    class Meta:
-        ordering = ['u_id']
+
+#u_id = models.CharField(max_length=20,blank=False, null= False, unique=True, primary_key= True)
+#pw = models.CharField(max_length=20,blank=False, null= False)
+
+#토큰
+#token = Token.objects.create(user=User)
--- LectBox_back/project/app/urls.py
+++ LectBox_back/project/app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from app import views
 
 urlpatterns = [
-    path('sign-up/', views.user_list),
-    path('users/<str:userid>', views.user_detail),
+    path('sign-up/', views.SignupView.as_view()),
+    path('member-detail/', views.user_detail),
+    path('login/', views.LoginView.as_view()),
 ]
+
+#유저 id 파라미터로 받는 형식
+#path('users/<str:userid>', views.user_detail)
