c71222dceab97692ea6281954686a90a336c9e27
GoByeonghu <byeonghugo@gmail.com>
2022-06-05 00:51:46 +0900

    Complet folder api

--- LectBox_back/project/app/serializers.py
+++ LectBox_back/project/app/serializers.py
@@ -22,3 +22,43 @@ class FolderSerializer(serializers.ModelSerializer):
         model = FolderItems
         fields = ['id', 'made_by', 'name', 'max_volume', 'volume', 'type']
 
+# 유저 모든 정보 포함
+class UserSerializer(serializers.ModelSerializer):
+    
+    def update(self, instance, validated_data):
+        instance.is_student = validated_data.get('is_student',instance.is_student)
+        instance.name = validated_data.get('name',instance.name)
+        instance.email = validated_data.get('email',instance.email)
+        instance.school = validated_data.get('school',instance.school)
+        instance.department = validated_data.get('department',instance.department)
+        instance.save()
+        return instance
+    
+    class Meta:
+        model = Users
+        fields = ['user', 'is_student', 'name', 'email', 'school', 'department']
+
+class FSerializer(serializers.ModelSerializer):
+
+    #items = serializers.
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name',instance.name)
+        instance.save()
+        return instance
+
+    class Meta:
+        model = FolderItems
+        fields = ['id', 'made_by', 'name', 'max_volume', 'volume', 'type']
+
+class FUSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Folder_User_Relationships
+        fields = ['folder_id', 'user_id','type']
+
+class ClassSerializer(serializers.ModelSerializer):
+    #class list
+    class Meta:
+        model = FolderItems
+        fields = ['id', 'made_by', 'name']
+
--- LectBox_back/project/app/urls.py
+++ LectBox_back/project/app/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('login/', views.LoginView.as_view()),
     path('folder/<int:f_id>', views.folder_detail),
     path('folder/<int:f_id>/<int:type>', views.folder_type),
+    path('folder_path/<int:f_id>', views.folder_path),
     path('folder/', views.folder_detail),
-    path('class/', views.user_detail),
+    path('class-list/', views.class_list),
+    path('class-entrance/<int:f_id>',views.class_entrance),
+  
 ]
